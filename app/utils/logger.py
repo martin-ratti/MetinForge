bf1513d3b579fe046b5b730bc7cab4bab6bae5b5
MartinUTN <martinratti10@gmail.com>
2026-02-08 20:27:01 -0300

    style: Improve log aesthetics and ignore logs directory

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -8,7 +8,8 @@ def setup_logger(name="MetinForge"):
     # Avoid adding handlers multiple times
     if not logger.handlers:
         logger.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        # Aesthetic format: [Time] | LEVEL    | Message
+        formatter = logging.Formatter('[%(asctime)s] | %(levelname)-8s | %(message)s', datefmt='%H:%M:%S')
         
         # Console Handler
         ch = logging.StreamHandler(sys.stdout)
@@ -22,6 +23,12 @@ def setup_logger(name="MetinForge"):
             fh = logging.FileHandler(os.path.join(log_dir, "app.log"))
             fh.setFormatter(formatter)
             logger.addHandler(fh)
+            
+            # Add session separator
+            fh.stream.write("\n" + "="*80 + "\n")
+            fh.stream.write(f" SESSION STARTED: {logging.Formatter('%(asctime)s').format(logging.LogRecord(None, None, None, None, None, None, None))}\n")
+            fh.stream.write("="*80 + "\n")
+            
         except Exception as e:
             print(f"Failed to setup file logging: {e}")
         
